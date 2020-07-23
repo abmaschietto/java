73e14d355a43e04da27736c42ae3e5e56927f021
Artur Boudoux <arturboudoux@gmail.com>
2020-07-23 16:22:41 -0300

    Cross Origin aberto

--- src/main/java/com/produtos/apirest/controllers/AuthController.java
+++ src/main/java/com/produtos/apirest/controllers/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.produtos.apirest.security.TokenDTO;
 
 @RestController
 @RequestMapping(value = "/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 	
 	@Autowired
